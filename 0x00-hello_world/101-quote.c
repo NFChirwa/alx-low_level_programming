6da18200396ea548653fa1ba7e86fcd608fc8276
NFChirwa <Nigelfaraichirwa@gmail.com>
2023-02-19 06:00:25 -0800

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * return; Always 1 (success)
+ */
+
+int main(void) 
+{
+	scanf("\"and that piece of art is useful" - Dora Korpar, 2015-10-19");
+		return (1);
+}
+
