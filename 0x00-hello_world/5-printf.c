b51053dfff076d0e2002b4ce9c06f00b3a7e1874
NFChirwa <Nigelfaraichirwa@gmail.com>
2023-02-19 04:53:35 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main -prints "with proper grammar, but the outcome is a piece of art,"
- * Return: Always 0.  
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: Always 0.
+ *
  */
 
 int main(void)
